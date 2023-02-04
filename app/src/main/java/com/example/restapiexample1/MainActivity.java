b5421cfc04cccf509524f8a7b63904a018eca6c2
mcrsr <mcrsr0209@gmail.com>
2023-02-04 16:09:42 +0530

    Second Commit

--- app/src/main/java/com/example/restapiexample1/MainActivity.java
+++ app/src/main/java/com/example/restapiexample1/MainActivity.java
@@ -33,16 +33,20 @@ public class MainActivity extends AppCompatActivity {
     public void getData(View view) {
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://jsonplaceholder.typicode.com/posts";
+        String url = "https://reqres.in/api/users";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        List<String> authors = JsonPath.read(response, "$.[*].body");
-                        ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.activity_listview,authors);
-                        ListView listView = (ListView) findViewById(R.id.listView);
-                        listView.setAdapter(adapter);
+                        List<String> first_name = JsonPath.read(response, "$.data[*].first_name");
+                        List<String> last_name = JsonPath.read(response, "$.data[*].last_name");
+                        List<String> avatar = JsonPath.read(response, "$.data[*].avatar");
+
+                        MyListAdapter adapter=new MyListAdapter(MainActivity.this, first_name.toArray(new String[0]), last_name.toArray(new String[0]),avatar.toArray(new String[0]));
+                        ListView list = (ListView) findViewById(R.id.list);
+                        list.setAdapter(adapter);
+
                     }
                 }, new Response.ErrorListener() {
             @Override
---
+++ app/src/main/java/com/example/restapiexample1/MyListAdapter.java
@@ -0,0 +1,47 @@
+package com.example.restapiexample1;
+
+import android.app.Activity;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] maintitle;
+    private final String[] subtitle;
+    private final String[] imgid;
+
+    public MyListAdapter(Activity context, String[] maintitle,String[] subtitle, String[] imgid) {
+        super(context, R.layout.activity_listview, maintitle);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.maintitle=maintitle;
+        this.subtitle=subtitle;
+        this.imgid=imgid;
+
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.activity_listview, null,true);
+
+        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
+
+        titleText.setText(maintitle[position]);
+        imageView.setImageURI(Uri.parse(imgid[position]));
+        subtitleText.setText(subtitle[position]);
+
+        return rowView;
+
+    };
+
+}
